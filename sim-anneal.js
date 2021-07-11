9118325fabb9253a3b67c9726dda972c2dd2178b
Snowsore <snow_sore@hotmail.com>
2021-07-11 13:09:22 +0800

    整理代码

--- sim-anneal.js
+++ sim-anneal.js
@@ -1,25 +1,35 @@
 (() => {
 
     window.simAnneal = {
-        setup(temp, rate, prev, generate, eval) {
+        setup(temp, rate, cur, generate, eval) {
+
+            var count = 1
 
             return {
                 step() {
 
-                    var next = generate(prev.map(x => x))
-                    var dE = eval(next) - eval(prev)
+                    var next = generate(cur)
+                    var nE = eval(next)
+                    var cE = eval(cur)
+                    var dE = nE - cE
 
                     if(dE < 0) {
-                        prev = next
+                        cur = next
                     } else {
                         if(Math.random() < Math.exp(-dE / temp)) {
-                            prev = next
+                            cur = next
                         }
                     }
         
                     temp = rate * temp
 
-                    return prev
+                    return {
+                        solution: cur,
+                        energy: cE,
+                        count: count++,
+                        temp: temp,
+                        rate: rate
+                    }
                 }
             }
         }
